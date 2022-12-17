bd4713a395fd625829715b3f0d7009b1bf288782
Steven Bird <steven.bird92@gmail.com>
2022-12-16 20:10:47 -0600

    Added Tweet classes derived from api documentation

---
+++ src/SCB.TwitterAnalyzer.Domain.Models/Entities.cs
@@ -0,0 +1,48 @@
+﻿
+namespace SCB.TwitterAnalyzer.Domain.Models
+{
+    public record Entities
+    {
+        public IEnumerable<Anotation> Anotations { get; init; } = Enumerable.Empty<Anotation>();
+        public IEnumerable<Url> Urls { get; init; } = Enumerable.Empty<Url>();
+        public IEnumerable<HashTag> HashTags { get; init; } = Enumerable.Empty<HashTag>();
+        public IEnumerable<Mention> Mentions { get; init; } = Enumerable.Empty<Mention>();
+
+    }
+
+    public abstract record Entity
+    {
+        public int Start { get; init; }
+        public int End { get; init; }
+    }
+
+    public record Anotation : Entity
+    {
+        public string Type { get; init; } = string.Empty;
+        public string NormalizedText { get;init; } = string.Empty;
+
+    }
+
+    public record Url : Entity
+    {
+        public string UserUrl { get; init; } = string.Empty;
+        public string ExpandedUrl { get; init; } = string.Empty;
+        public string DisplayUrl { get; init; } = string.Empty;
+        public string UnwoundUrl { get; init; } = string.Empty;
+    }
+
+    public record HashTag : Entity
+    {
+        public string Tag { get; init; }
+    }
+
+    public record Mention : Entity
+    {
+        public string UserName { get; init; } = string.Empty;
+    }
+
+    public record CashTag : Entity
+    {
+        public string Tag { get; init; }
+    }
+}
---
+++ src/SCB.TwitterAnalyzer.Domain.Models/Tweet.cs
@@ -0,0 +1,14 @@
+﻿namespace SCB.TwitterAnalyzer.Domain.Models
+{
+    public record Tweet
+    {
+        public string Id { get; init; } = string.Empty;
+        public string Text { get; init; } = string.Empty;
+        public DateTime? CreatedAt { get; init; } 
+        public IEnumerable<string> EditHistoryTweetIds { get; init; } = Enumerable.Empty<string>();
+        public Entities? Entities { get; init; }
+        public string Language { get; init; } = string.Empty;
+        public string Source { get; init; } = string.Empty;
+        public bool PossiblySensitive { get; init; }
+    }
+}
