61c43d7fbdfff3b3071d84153dda2cded219568a
Steven Bird <steven.bird92@gmail.com>
2022-12-19 07:14:24 -0600

    Added testing for the sample stream service. Simple queue client to raise event on enqueuing to a concurrent queue

---
+++ src/SCB.TwitterAnalyzer.DomainServices/ITweetQueue.cs
@@ -0,0 +1,10 @@
+﻿using SCB.TwitterAnalyzer.Domain.Models;
+
+namespace SCB.TwitterAnalyzer.Domain.Services
+{
+    public interface ITweetQueue
+    {
+        event EventHandler TweetEnqueued;
+        void Enqueue(Tweet tweet);
+    }
+}
